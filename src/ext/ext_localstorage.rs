73bdb9c8ea2eedabaa20d5f88851eb66673efd0f
kason <me@kason.site>
2024-09-17 22:31:30 +0800

    optimize localstorage flushing

--- src/ext/ext_localstorage.rs
+++ src/ext/ext_localstorage.rs
@@ -1,6 +1,8 @@
+use std::cell::RefCell;
 use anyhow::Error;
 use lazy_static::lazy_static;
 use crate::data_dir::get_data_path;
+use crate::timer::{set_timeout, TimerHandle};
 lazy_static! {
     static ref DB: sled::Db = {
         let dir = get_data_path("localstorage");
@@ -8,8 +10,16 @@ lazy_static! {
     };
 }
 
+thread_local! {
+    static FLUSH_TIMER_HANDLE : RefCell<Option<TimerHandle>> = RefCell::new(None);
+}
+
 pub fn localstorage_set(key: String, value: String) -> Result<(), Error> {
     DB.insert(key, value.as_bytes())?;
+    let flush_timer_handle = set_timeout(|| {
+        let _ = localstorage_flush();
+    }, 1000);
+    FLUSH_TIMER_HANDLE.replace(Some(flush_timer_handle));
     Ok(())
 }
 
