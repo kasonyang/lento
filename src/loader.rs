1f551abe570f03ef9c357a864617990018460af2
kason <me@kason.site>
2024-09-18 21:34:35 +0800

    add DefaultModuleLoader

--- src/loader.rs
+++ src/loader.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::io::{Error, ErrorKind};
 use anyhow::anyhow;
-use quick_js::loader::JsModuleLoader;
+use quick_js::loader::{FsJsModuleLoader, JsModuleLoader};
 
 pub struct RemoteModuleLoader {
     base: String,
@@ -49,5 +49,43 @@ impl JsModuleLoader for StaticModuleLoader {
             None => Err(Error::new(ErrorKind::NotFound, anyhow!("Not found"))),
             Some(s) => Ok(s.to_string())
         }
+    }
+}
+
+pub struct DefaultModuleLoader {
+    remote_module_loader: Option<RemoteModuleLoader>,
+    fs_module_loader: Option<FsJsModuleLoader>,
+}
+
+impl DefaultModuleLoader {
+
+    pub fn new() -> Self {
+        Self {
+            remote_module_loader: None,
+            fs_module_loader: None,
+        }
+    }
+
+    pub fn set_fs_base(&mut self, dir: &str) {
+        self.fs_module_loader = Some(FsJsModuleLoader::new(dir))
+    }
+
+    pub fn set_remote_base(&mut self, base: &str) {
+        self.remote_module_loader = Some(RemoteModuleLoader::new(base.to_string()))
+    }
+
+}
+
+impl JsModuleLoader for DefaultModuleLoader {
+    fn load(&self, module_name: &str) -> Result<String, Error> {
+        if let Some(fs_loader) = &self.fs_module_loader {
+            if let Ok(module) = fs_loader.load(module_name) {
+               return Ok(module)
+            }
+        }
+        if let Some(remote_loader) = &self.remote_module_loader {
+            return remote_loader.load(module_name);
+        }
+        Err(Error::new(ErrorKind::NotFound, "failed to load module"))
     }
 }
